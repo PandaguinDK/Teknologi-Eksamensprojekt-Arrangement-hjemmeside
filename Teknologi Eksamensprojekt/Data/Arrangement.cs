d49ce0b0e9d9617f6f391ead35abbc7d37c827c5
Martin <martin@guidmaster.dk>
2024-05-03 14:00:24 +0200

    Add project files.

---
+++ Teknologi Eksamensprojekt/Data/Arrangement.cs
@@ -0,0 +1,16 @@
+﻿namespace Teknologi_Eksamensprojekt.Data
+{
+    public class Arrangement
+    {
+        public Arrangement(string name, DateTime date, string description)
+        {
+            this.Name = name;
+            this.Date = date;
+            this.Description = description;
+        }
+
+        public string Name { get; set; }
+        DateTime Date { get; set; }
+        public string Description { get; set; }
+    }
+}
